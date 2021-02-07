20fc14fb223934c2904743cca127cd31e96dd036
frankpagan <frank@cocreate.app>
2021-02-07 23:29:52 +0000

    test doc

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -9,7 +9,7 @@ module.exports = {
     sources: [{
             path: "./docs/index.html",
             collection: "files",
-            document_id: "",
+            document_id: "60206d5b3c260207bf49cab4",
             key: "html",
             data:{
                 name: "Modal Doc",
@@ -19,10 +19,10 @@ module.exports = {
    
     crud: [{
             collection: "routes",
-            document_id: "",
+            document_id: "60206d5b3c260207bf49cab3",
             data:{
                 collection: "files",
-                document_id: "",
+                document_id: "60206d5b3c260207bf49cab4",
                 name: "html",
                 domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
                 route: "/docs/modal",
