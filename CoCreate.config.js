a49b50b0bc6eaa4fd5cb0cd9896a2ad829b05588
frank <frank@cocreate.app>
2021-05-03 01:19:06 +0000

    docs: CoCreate-hosting package to publish docs

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -7,7 +7,7 @@ module.exports = {
 	},
 
     "sources": [{
-            "path": "./docs/index.html",
+            "entry": "./docs/index.html",
             "collection": "files",
             "document_id": "60206d5b3c260207bf49cab4",
             "key": "src",
