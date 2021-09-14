b81b24a06775adeb6339077439da6ed47df5f0eb
frank pagan <frank@cocreate.app>
2021-09-14 03:55:39 +0000

    fix: add cocreate.app to domains

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -14,6 +14,7 @@ module.exports = {
                 "name": "index.html",
                 "path": "/docs/modal/index.html",
                 "domains": [
+                    "cocreate.app",
                     "general.cocreate.app"
                 ],
                 "directory": "/docs/modal",
