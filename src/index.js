c09573779bc81c8ae8efcd6b39529281d9a9d183
Frank Pagan <frank@cocreate.app>
2022-12-27 13:23:02 +0000

    feat: href replace with modal-src, open-modal action

--- src/index.js
+++ src/index.js
@@ -6,224 +6,232 @@ import message from '@cocreate/message-client';
 import './index.css';
 
 function CoCreateModal(id) {
-  this.viewPort = null;
-  this.id = id || 'modal-viewport'
-  
-  this.pageId = uuid.generate();
-  this.isRoot = this._checkRoot();
-  
-  if (this.isRoot) {
-    this.viewPorts = new Map();
-    this.viewPorts.set(this.pageId, this)
-    this.modals = new Map();
-    this.rootId = this.pageId;
-    this.parentId = this.pageId;
-    window.localStorage.setItem('rootId', this.pageId)
-  } else {
-    this.pageId = window.localStorage.getItem('pageId')
-    let viewPort = window.top.CoCreate.modal.viewPorts
-    if (viewPort) 
-      viewPort.set(this.pageId, this)
-    
-    // ToDo:  can be depreciated if we find a better way to pass rootid and parentid
-    this.parentId = window.localStorage.getItem('parentId')
-    this.rootId = window.top.CoCreate.modal.rootId
-  } 
-  
-  this._createViewPort();
-  
-  this._initSocket();
-  
-  action.init({
-    name: "openModal",
-    endEvent: "openModal",
-    callback: (btn, data) => {
-      this.open(btn);
-    },
-  });
+    this.viewPort = null;
+    this.id = id || 'modal-viewport'
 
-  action.init({
-    name: "closeModal",
-    endEvent: "closeModal",
-    callback: (btn, data) => {
-      this.modalAction(btn, 'close');
-    },
-  });
+    this.pageId = uuid.generate();
+    this.isRoot = this._checkRoot();
 
-  action.init({
-    name: "minMaxModal",
-    endEvent: "minMaxModal",
-    callback: (btn, data) => {
-      this.modalAction(btn, 'minMax');
-    },
-  });
+    if (this.isRoot) {
+        this.viewPorts = new Map();
+        this.viewPorts.set(this.pageId, this)
+        this.modals = new Map();
+        this.rootId = this.pageId;
+        this.parentId = this.pageId;
+        window.localStorage.setItem('rootId', this.pageId)
+    } else {
+        this.pageId = window.localStorage.getItem('pageId')
+        let viewPort = window.top.CoCreate.modal.viewPorts
+        if (viewPort) 
+            viewPort.set(this.pageId, this)
+        
+        // ToDo:  can be depreciated if we find a better way to pass rootid and parentid
+        this.parentId = window.localStorage.getItem('parentId')
+        this.rootId = window.top.CoCreate.modal.rootId
+    } 
 
-  action.init({
-    name: "parkModal",
-    endEvent: "parkModal",
-    callback: (btn, data) => {
-      this.modalAction(btn, 'park');
-    },
-  });
-  
+    this._createViewPort();
+
+    this._initSocket();
+
+    action.init({
+        name: "openModal",
+        endEvent: "openModal",
+        callback: (btn, data) => {
+            this.open(btn);
+        },
+    });
+
+    action.init({
+        name: "closeModal",
+        endEvent: "closeModal",
+        callback: (btn, data) => {
+            this.modalAction(btn, 'close');
+        },
+    });
+
+    action.init({
+        name: "minMaxModal",
+        endEvent: "minMaxModal",
+        callback: (btn, data) => {
+            this.modalAction(btn, 'minMax');
+        },
+    });
+
+    action.init({
+        name: "parkModal",
+        endEvent: "parkModal",
+        callback: (btn, data) => {
+            this.modalAction(btn, 'park');
+        },
+    });
+
 }
 
 CoCreateModal.prototype = {
-  _checkRoot: function() {
-    try {
-        return window === window.top;
-    } catch (e) {
-        return false;
-    }
-  },
+    _checkRoot: function() {
+        try {
+            return window === window.top;
+        } catch (e) {
+            return false;
+        }
+    },
 
-  _createViewPort: function() {
-    if (this.viewPort) 
-      return true;
+    _createViewPort: function() {
+        if (this.viewPort) 
+            return true;
 
-    var el = document.getElementById(this.id);
-    if (el) {
-      this.viewPort = new ModalViewPort(el);
-      return true;
-    } else {
-      return false;
-    }
-  },
-  
-  _initSocket: function() {
-    var self = this;    
-    message.listen('modalAction', function(response) {
-        self.runModalAction(response.data)
-    })
-  },
-  
-  open: function(aTag) {
-    let attributes = [];
-    for (let attribute of aTag.attributes)
-      attributes[attribute.name] = attribute.value
+        let el = document.getElementById(this.id);
+        if (el) {
+            this.viewPort = new ModalViewPort(el);
+            return true;
+        } else {
+            return false;
+        }
+    },
 
-    let data = {
-      url:    aTag.href || aTag.getAttribute('href'),
-      x:      aTag.getAttribute('modal-x'),
-      y:      aTag.getAttribute('modal-y'),
-      width:  aTag.getAttribute('modal-width'),
-      height: aTag.getAttribute('modal-height'),
-      color:  aTag.getAttribute('modal-color'),
-      header: aTag.getAttribute('modal-header'), 
-      attributes: attributes
-    }
-    
-    let openIn = aTag.getAttribute('modal-open') || 'root';
+    _initSocket: function() {
+        const self = this;    
+        message.listen('modalAction', function(response) {
+            self.runModalAction(response.data)
+        })
+    },
+
+    open: function(aTag) {
+        let attributes = [];
+        for (let attribute of aTag.attributes)
+            attributes[attribute.name] = attribute.value
+
+        let data = {
+            src:    aTag.getAttribute('modal-src'),
+            x:      aTag.getAttribute('modal-x'),
+            y:      aTag.getAttribute('modal-y'),
+            width:  aTag.getAttribute('modal-width'),
+            height: aTag.getAttribute('modal-height'),
+            color:  aTag.getAttribute('modal-color'),
+            header: aTag.getAttribute('modal-header'), 
+            iframe: aTag.getAttribute('modal-iframe'), 
+            attributes: attributes
+        }
+
+        // retrieves absolute path of href
+        if (data.src) {
+            let a = document.createElement('a')
+            a.href = data.src
+            data.src = a.href
+            a.remove
+        }
+
+        let openIn = aTag.getAttribute('modal-open') || 'root';
+            
+        let openId;
+        switch (openIn) {
+        case 'parent':
+            openId = this.parentId;
+            break;
+        case 'page':
+            openId = this.pageId;
+            break;
+        case 'root':
+            openId = this.rootId;
+            break;
+        default:
+            openId = openIn;
+            break;
+        }
+
+        window.localStorage.setItem('parentId', openId)
         
-    let openId;
-    switch (openIn) {
-      case 'parent':
-        openId = this.parentId;
-        break;
-      case 'page':
-        openId = this.pageId;
-        break;
-      case 'root':
-        openId = this.rootId;
-        break;
-      default:
-        openId = openIn;
-        break;
+        data.type = 'open';
+        data.parentId = openId;
+
+        if (this.isRoot) {
+            if (this._createViewPort())
+                this.viewPort._createModal(data);  
+        } else {
+            let viewPort = window.top.CoCreate.modal.viewPorts.get(openId);
+            if (viewPort.pageId == data.parentId) {
+                viewPort.viewPort._createModal(data);
+
+                document.dispatchEvent(new CustomEvent('openModal', {
+                detail: {}
+                }))
+
+            } else {
+                this.modalAction(btn, 'open', data)
+            }
+        }
+    },
+
+    modalAction: function(btn, type, data) {
+        let json = {
+            apiKey: CoCreateConfig.apiKey,
+            organization_id: CoCreateConfig.organization_id,
+            broadcastSender: true,
+            message: 'modalAction',
+        }
+
+        if (type == 'open') {
+            json.data = data
+            message.send(json);
+        } else {
+            let modalEl = btn.closest('.modal')
+
+        let modal
+        if (modalEl)
+            modal = this.viewPort.modals.get(modalEl.id || this.pageId)
+        if (!modal) {
+            modal = window.top.CoCreate.modal.modals.get(this.pageId);
+        }
+        json.data = {
+            parentId: this.parentId,
+            pageId: this.pageId,
+            modal,
+            type
+        }
+
+        if (modal)
+            this.runModalAction(json.data)
+        else
+            message.send(json);
+        }
+    },
+
+    runModalAction: function(data) {
+        if (data.modal || data.parentId == this.pageId) {
+            
+            let pageId = data.pageId;
+            let type = data.type;
+            let modal = data.modal
+
+            if (!modal)
+                modal = this.viewPort.modals.get(pageId);
+            
+            if (modal) {
+                switch (type) {
+                case 'open':
+                    if (data.parentId == this.pageId)
+                        this.viewPort._createModal(data)          
+                    break;
+                case 'close':
+                    modal.viewPort._removeModal(modal)
+                    break;
+                case 'minMax':
+                    modal.el.position.minMax();
+                    break;
+                case 'park':
+                    modal.togglePark();
+                    break;
+                } 
+
+                document.dispatchEvent(new CustomEvent(`${type}Modal`, {
+                    detail: {}
+                }))
+                    
+            }
+        }
+
     }
-
-    window.localStorage.setItem('parentId', openId)
-    
-    data.type = 'open';
-    data.parentId = openId;
-
-    if (this.isRoot) {
-      if (this._createViewPort())
-        this.viewPort._createModal(data);  
-    } else {
-      let viewPort = window.top.CoCreate.modal.viewPorts.get(openId);
-      if (viewPort.pageId == data.parentId) {
-        viewPort.viewPort._createModal(data);
-
-        document.dispatchEvent(new CustomEvent('openModal', {
-          detail: {}
-        }))
-
-      } else {
-        this.modalAction(btn, 'open', data)
-      }
-    }
-  },
-
-  modalAction: function(btn, type, data) {
-    let json = {
-      apiKey: CoCreateConfig.apiKey,
-      organization_id: CoCreateConfig.organization_id,
-      broadcastSender: true,
-      message: 'modalAction',
-    }
-
-    if (type == 'open') {
-      json.data = data
-      message.send(json);
-    } else {
-      let modalEl = btn.closest('.modal')
-
-      let modal
-      if (modalEl)
-        modal = this.viewPort.modals.get(modalEl.id || this.pageId)
-      if (!modal) {
-        modal = window.top.CoCreate.modal.modals.get(this.pageId);
-      }
-      json.data = {
-        parentId: this.parentId,
-        pageId: this.pageId,
-        modal,
-        type
-      }
-
-      if (modal)
-        this.runModalAction(json.data)
-      else
-        message.send(json);
-    }
-  },
-
-  runModalAction: function(data) {
-    if (data.modal || data.parentId == this.pageId) {
-        
-      var pageId = data.pageId;
-      var type = data.type;
-      var modal = data.modal
-
-      if (!modal)
-        modal = this.viewPort.modals.get(pageId);
-      
-      if (modal) {
-        switch (type) {
-          case 'open':
-            if (data.parentId == this.pageId)
-              this.viewPort._createModal(data)          
-            break;
-          case 'close':
-            modal.viewPort._removeModal(modal)
-            break;
-          case 'minMax':
-            modal.el.position.minMax();
-            break;
-          case 'park':
-            modal.togglePark();
-            break;
-          default:
-        } 
-
-        document.dispatchEvent(new CustomEvent(`${type}Modal`, {
-          detail: {}
-        }))
-              
-      }
-    }
-
-  }
 
 }
 
--- src/modal.js
+++ src/modal.js
@@ -2,208 +2,196 @@ import uuid from '@cocreate/uuid'
 import '@cocreate/position'
 
 function Modal(el, options, viewPort) {
-  if (!(el && el.nodeType && el.nodeType === 1)) {
-		return;
-	}
-		
-	/** options **/
-	let defaults = {
-	  minWidth: 60,
-	  minHeight: 40,
-	};
-	
-	this.id = uuid.generate();
-  this.viewPort = viewPort;
-	this.el = el;
+    if (!(el && el.nodeType && el.nodeType === 1)) {
+        return;
+    }
+        
+    /** options **/
+    let defaults = {
+        minWidth: 60,
+        minHeight: 40,
+    };
+    
+    this.id = uuid.generate();
+    this.viewPort = viewPort;
+    this.el = el;
 
-	this.isHeader = true;
-	this.headerArea = null;
-  this.isParked = false;	
+    this.hasHeader = true;
+    this.header = null;
+    this.isParked = false;	
 
-	this.width = 0;
-	this.height = 0;
-	
-	this.frame = null;
+    this.width = 0;
+    this.height = 0;
+    
+    this.frame = null;
 
-	this.options = Object.assign(defaults, options);
-	
-	this.el.setAttribute("id", this.id);
-	this.el.setAttribute("moveable", '');
-	
-  window.localStorage.setItem('pageId', this.id)
-  
-	this._init();
-	this._setModalSize();
+    this.options = Object.assign(defaults, options);
+    
+    this.el.setAttribute("id", this.id);
+    this.el.setAttribute("moveable", '');
+    
+    window.localStorage.setItem('pageId', this.id)
+
+    this._init();
+    this._setModalSize();
 }
 
 Modal.prototype = {
-  constructor: Modal,
-  
-  _init : function() {
-    var opt = this.options;
+    constructor: Modal,
     
-    var windowURL = opt.url   ? opt.url     : this.el.getAttribute("href");
-    var width =     opt.width ? opt.width   : this.el.getAttribute("modal-width");
-    var height =    opt.height? opt.height  : this.el.getAttribute("modal-height");
-    var color =     opt.color ? opt.color   : this.el.getAttribute("modal-color");
-    var x =         opt.x     ? opt.x       : this.el.getAttribute("modal-x")
-    var y =         opt.y     ? opt.y       : this.el.getAttribute("modal-y")
-    var showHeader= opt.header? opt.header  : this.el.getAttribute("modal-header")
-    
-    var attributes = opt.attributes;
+    _init : function() {
+        let opt = this.options;
         
-    if (width && width != "") {
-      this.el.style.width = width;
-    } else {
-      this.el.style.width = "100%";
-    }
-    if (height && height != "") {
-      this.el.style.height = height;
-    } else {
-      this.el.style.height = "100%";
-    }
-    
-    if (this.el.parentElement.clientWidth < this.el.clientWidth) {
-      this.el.style.width = "100%"
-    }
-    if (this.el.parentElement.clientHeight < this.el.clientHeight) {
-      this.el.style.width = "100%"
-    }
-
-
-    if (x && x != "") {
-      this.el.style.left = x;
-    } else {
-      let hX = (this.el.parentElement.clientWidth - this.el.offsetWidth) / 2;
-      hX = hX > 0 ? hX : 0;
-      this.el.style.left = hX + "px";
-    }
-    if (y && y != "") {
-      this.el.style.top = y;
-    } else {
-      let hY = (this.el.parentElement.clientHeight - this.el.offsetHeight) / 2
-      hY = hY > 0 ? hY : 0
-      this.el.style.top = hY + "px";
-    }
-    if (color && color !== "") this.el.style.borderColor = color;
-    
-    if (showHeader == "true") {
-      this.isHeader = true;
-      this._createTitle();
-    } else {
-      this.isHeader = false;
-      this._createDragArea();
-    }
-    
-    this.el.innerHTML = this.el.innerHTML + `<div class="parked-closeBtn"><i class="fas fa-times closeBtn"></i></div>`;
-    
-    let frame = null;
-    if (windowURL && windowURL != "") {
-      frame = this.__createFrame(this.headerArea);
-      frame.src = windowURL;
-    } else {
-      frame = this.__createFrame(this.headerArea, "div");
-      frame.classList.add('overflow:auto')
-      frame.classList.add('height:100vh')
-    }
-
-    if (attributes){
-      if (attributes['pass_to']) {
-        frame.setAttribute('pass_id', attributes['pass_to'].value);
-      }
-      for (let attribute of attributes){
-        if (attribute.name.startsWith('pass-')){
-          frame.setAttribute(`${attribute.name.substring(5)}`, attribute.value);
+        let src =        opt.src   ? opt.src     : this.el.getAttribute("modal-src");
+        let width =      opt.width ? opt.width   : this.el.getAttribute("modal-width");
+        let height =     opt.height? opt.height  : this.el.getAttribute("modal-height");
+        let color =      opt.color ? opt.color   : this.el.getAttribute("modal-color");
+        let x =          opt.x     ? opt.x       : this.el.getAttribute("modal-x")
+        let y =          opt.y     ? opt.y       : this.el.getAttribute("modal-y")
+        let header = opt.header? opt.header  : this.el.getAttribute("modal-header")
+        let iframe = opt.iframe? opt.iframe  : this.el.getAttribute("modal-iframe")
+        
+        let attributes = opt.attributes;
+            
+        if (width && width != "") {
+            this.el.style.width = width;
+        } else {
+            this.el.style.width = "100%";
         }
-      }  
+        if (height && height != "") {
+            this.el.style.height = height;
+        } else {
+            this.el.style.height = "100%";
+        }
+        
+        if (this.el.parentElement.clientWidth < this.el.clientWidth) {
+            this.el.style.width = "100%"
+        }
+        if (this.el.parentElement.clientHeight < this.el.clientHeight) {
+            this.el.style.width = "100%"
+        }
+
+        if (x && x != "") {
+            this.el.style.left = x;
+        } else {
+            let hX = (this.el.parentElement.clientWidth - this.el.offsetWidth) / 2;
+            hX = hX > 0 ? hX : 0;
+            this.el.style.left = hX + "px";
+        }
+        if (y && y != "") {
+            this.el.style.top = y;
+        } else {
+            let hY = (this.el.parentElement.clientHeight - this.el.offsetHeight) / 2
+            hY = hY > 0 ? hY : 0
+            this.el.style.top = hY + "px";
+        }
+        if (color && color !== "") 
+            this.el.style.borderColor = color;
+                        
+        let frame = null;
+        if (iframe && iframe != 'false' || iframe == '') {
+            frame = document.createElement('iframe')
+            if (src)
+                frame.src = src;
+        } else {
+            frame = document.createElement('modal-container');
+            if (src)
+                frame.setAttribute('src', src);
+        }
+
+        if (attributes){
+            if (attributes['pass_to']) {
+                frame.setAttribute('pass_id', attributes['pass_to'].value);
+            }
+            for (let attribute of attributes){
+                if (attribute.name.startsWith('pass-')){
+                    frame.setAttribute(`${attribute.name.substring(5)}`, attribute.value);
+                }
+            }  
+        }
+
+        if (header && header != "false"  || header == '') {
+            this.hasHeader = true;
+            frame.classList.add('has-modal-header')
+            this._createHeader(header);
+        } else {
+            this.hasHeader = false;
+            this._createDragArea();
+        }
+        
+        this.el.appendChild(frame)
+        this.frame = frame;
+
+        let self = this;
+        this.el.addEventListener("dblclick", function(e) {
+            if (self.isParked) {
+                self.isParked = false;
+                self.el.classList.remove("modal-parked");
+            } 
+        })
+
+    },        
+            
+    _createHeader: function(header) {
+        let headerTemplate
+        if (/<\/?[a-z][\s\S]*>/i.test(header))
+            headerTemplate = header
+        else
+            headerTemplate = `<div class="nav bg-light-gray"><ul class="modal-header">
+                <li><a class="minimizeBtn"><i class="far fa-window-minimize"></i></a></li>
+                <li><a class="maximizeBtn"><i class="far fa-window-restore"></i></a></li>
+                <!-- <li><a class="parkBtn"><i class="fas fa-dot-circle "></i></a></li> -->
+                <li><a class="closeBtn"><i class="fas fa-times"></i></a></li>
+                </ul></div>`;
+        
+        this.el.innerHTML = headerTemplate + this.el.innerHTML;
+        this.header = this.el.querySelector('.modal-header');
+    },
+    
+    _createDragArea: function() {
+        let dragArea = document.createElement("div");
+        dragArea.classList.add("modal-drag-area");
+        
+        let topResize = document.createElement("div");
+        topResize.setAttribute("resize", "top");
+
+        let leftResize = document.createElement("div");
+        leftResize.setAttribute("resize", "left");
+
+        let rightResize = document.createElement("div");
+        rightResize.setAttribute("resize", "right");
+
+        let bottomResize = document.createElement("div");
+        bottomResize.setAttribute("resize", "bottom");
+
+        this.el.setAttribute("resizable", "");
+        this.el.appendChild(dragArea);
+        this.el.appendChild(topResize);
+        this.el.appendChild(leftResize);
+        this.el.appendChild(rightResize);
+        this.el.appendChild(bottomResize);
+    },
+
+    _setModalSize: function() {
+        let bound = this.el.getBoundingClientRect();
+        let parentBound = this.el.parentNode.getBoundingClientRect();
+        this.width = bound.width;
+        this.height = bound.height;
+        
+        this.el.style.left = bound.left - parentBound.left;
+        this.el.style.top = bound.top - parentBound.top;
+    },
+
+    togglePark: function() {
+        if (this.isParked) {
+            this.isParked = false;
+            this.el.classList.remove("modal-parked");
+        } else {
+            this.isParked = true;
+            this.el.classList.add("modal-parked")
+        }
     }
     
-    this.el.appendChild(frame)
-    this.frame = frame;
-
-    let self = this;
-    this.el.addEventListener("dblclick", function(e) {
-      if (self.isParked) {
-        self.isParked = false;
-        self.el.classList.remove("modal-parked");
-      } 
-    })
-
-  },
-  
-  __createFrame: function(isHeader, type) {
-    const tag = type || "iframe";
-    
-    let frame = document.createElement(tag);
-    frame.style.width = "100%";
-    frame.style.height = "100%";
-    if (isHeader) {
-      frame.style.height = "calc(100% - 45px)";
-    }
-    
-    if (type != "iframe") {
-      frame.setAttribute('class', 'domEditor');
-    }
-    return frame;
-  },
-    
-  
-  _setModalSize: function() {
-    let bound = this.el.getBoundingClientRect();
-    let parentBound = this.el.parentNode.getBoundingClientRect();
-    this.width = bound.width;
-    this.height = bound.height;
-    
-    this.el.style.left = bound.left - parentBound.left;
-    this.el.style.top = bound.top - parentBound.top;
-  },
-    
-  _createTitle: function(n) {
-    var header_template = `<div class="nav bg-light-gray"><ul class="modal-title-area">
-          <li><a class="minimizeBtn"><i class="far fa-window-minimize"></i></a></li>
-          <li><a class="maximizeBtn"><i class="far fa-window-restore"></i></a></li>
-         <!-- <li><a class="parkBtn"><i class="fas fa-dot-circle "></i></a></li> -->
-          <li><a class="closeBtn"><i class="fas fa-times"></i></a></li>
-      </ul></div>`;
-    this.el.innerHTML = header_template + this.el.innerHTML;
-    this.headerArea = this.el.querySelector('.modal-title-area');
-  },
-  
-  _createDragArea: function() {
-    let dragArea = document.createElement("div");
-    dragArea.classList.add("modal-drag-area");
-    
-    let top_area = document.createElement("div");
-    top_area.setAttribute("resize", "top");
-
-    let left_area = document.createElement("div");
-    left_area.setAttribute("resize", "left");
-
-    let right_area = document.createElement("div");
-    right_area.setAttribute("resize", "right");
-    right_area.classList.add("right:-5px!important");
-
-    let bottom_area = document.createElement("div");
-    bottom_area.setAttribute("resize", "bottom");
-
-    this.el.setAttribute("resizable", "");
-    this.el.appendChild(dragArea);
-    this.el.appendChild(top_area);
-    this.el.appendChild(left_area);
-    this.el.appendChild(right_area);
-    this.el.appendChild(bottom_area);
-  },
-
-  togglePark: function() {
-    if (this.isParked) {
-      this.isParked = false;
-      this.el.classList.remove("modal-parked");
-    } else {
-      this.isParked = true;
-      this.el.classList.add("modal-parked")
-    }
-  }
- 
 }
 
 export default Modal;
